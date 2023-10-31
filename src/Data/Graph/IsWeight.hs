cf89ddb34137bc96338099aaa898f340661e0f17
Rune K. Svendsen <runesvend@gmail.com>
2023-10-31 16:47:54 +0100

    WIP: working IsWeight (pre "m" removal)

--- src/Data/Graph/IsWeight.hs
+++ src/Data/Graph/IsWeight.hs
@@ -3,44 +3,53 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Data.Graph.IsWeight
 ( IsWeight(..)
-, Boxed, Unboxed
+, Boxed(..), Unboxed(..)
 )
 where
 
 import qualified Data.Array.ST as Arr
 import qualified Control.Monad.ST as Strict
 import qualified Control.Monad.ST.Lazy as Lazy
-import Data.Kind (Type)
-import Data.Proxy (Proxy)
 import qualified Data.Array.ST as ST
 
-data Boxed a
-data Unboxed a
+newtype Boxed a = Boxed { getBoxed :: a }
+newtype Unboxed a = Unboxed { getUnboxed :: a }
 
--- | A weight that can be used with the shortest path algorithms defined in this library.
+-- | A weight that can be used with the shortest path algorithms implemented by this library.
 --
 --   This is necessary to allow both:
 --      (a) arbitrary edge weights
 --      (b) better performance for unboxed edge weights (e.g. 'Int', 'Double')
-class IsWeight weight s (m :: Type -> Type) where
-    type Array weight
+class IsWeight weight s m | m -> s where
+    type Array weight s
 
-    newArray :: (Int, Int) -> weight -> m (Array weight)
-    getBounds :: Proxy weight -> Array weight -> m (Int, Int)
-    writeArray :: Array weight -> Int -> weight -> m ()
-    readArray :: Proxy weight -> Array weight -> Int -> m a
+    newArray :: (Int, Int) -> weight -> m (Array weight s)
+    getBounds :: Array weight s -> m (Int, Int)
+    writeArray :: Array weight s -> Int -> weight -> m ()
+    readArray :: Array weight s -> Int -> m weight
 
 instance Arr.MArray (Arr.STUArray s) weight (Strict.ST s) => IsWeight (Unboxed weight) s (Strict.ST s) where
-    type Array weight = ST.STUArray s Int weight
+    type Array (Unboxed weight) s = ST.STUArray s Int weight
+    newArray range (Unboxed weight) = ST.newArray range weight
+    getBounds = ST.getBounds
+    writeArray array idx (Unboxed weight) = ST.writeArray array idx weight
+    readArray array idx  = Unboxed <$> ST.readArray array idx
 
-    newArray = ST.newArray
+instance IsWeight (Boxed weight) s (Strict.ST s) where
+    type Array (Boxed weight) s = ST.STArray s Int weight
+    newArray range (Boxed weight) = ST.newArray range weight
+    getBounds = ST.getBounds
+    writeArray array idx (Boxed weight) = ST.writeArray array idx weight
+    readArray array idx  = Boxed <$> ST.readArray array idx
 
-    getBounds _ = ST.getBounds
-    writeArray = ST.writeArray
-    readArray _ = ST.readArray
-
-instance IsWeight (Boxed weight) s (Strict.ST s)
-instance IsWeight (Boxed weight) s (Lazy.ST s)
+instance IsWeight (Boxed weight) s (Lazy.ST s) where
+    type Array (Boxed weight) s = ST.STArray s Int weight
+    newArray range (Boxed weight) = ST.newArray range weight
+    getBounds = ST.getBounds
+    writeArray array idx (Boxed weight) = ST.writeArray array idx weight
+    readArray array idx  = Boxed <$> ST.readArray array idx
